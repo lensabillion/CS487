5b692c587d123858a0f84863611d9a65ba975b4f
lensabillion <56606537+lensabillion@users.noreply.github.com>
2020-02-14 01:12:16 +0300

    Add files via upload

---
+++ MaximumLengthOfPairChain.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findLongestChain(self, pairs):
+        """
+        :type pairs: List[List[int]]
+        :rtype: int
+        """
+        pairs.sort(key = lambda x:x[1])
+        
+        cnt,i = 0,0
+        for j in range(len(pairs)):
+            if  j == 0 or pairs[i][1] < pairs[j][0] :
+                cnt += 1
+                i = j
+                
+        return  cnt
