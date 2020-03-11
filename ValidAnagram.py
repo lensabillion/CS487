098077fef31e851ad99c1ae69b6de99bc9460f73
lensabillion <56606537+lensabillion@users.noreply.github.com>
2020-03-11 03:32:32 +0300

    Add files via upload

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,30 +1,11 @@
-###use Counting Sort###
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
 
-s="car"
-t="rat"
-AS=[]
-AT=[]
-SC=""
-TC=""
-for k in range(0,len(s)):
-    AS.append(s[k])
-    AT.append(t[k])
-
-for i in range(0,len(AS)):
-    for j in range(0,i):
-        if AS[j]>AS[i]:
-            temp = AS[i]
-            AS[i]=AS[j]
-            AS[j]=temp
-        if AT[j]>AT[i]:
-            temp = AT[i]
-            AT[i]=AT[j]
-            AT[j]=temp
-for i in range(0,len(AS)):
-    SC =SC +AS[i]
-    TC =TC +AT[i]
-if SC == TC:
-    print("true")
-else:
-    print("false")
-
+        if not s and not t:
+            return True
+        return sorted(s) == sorted(t)
